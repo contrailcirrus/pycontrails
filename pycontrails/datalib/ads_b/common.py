8e8144735a54fb393c3c331e8a7583cc8816f6b5
rogerteoh <rt415@ic.ac.uk>
2023-03-29 18:57:28 +0100

    Separate unique flights by metadata

---
+++ pycontrails/datalib/ads_b/common.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+# TODO: Interpolate between waypoints with dt < threshold
+# TODO: Find nearest airport
+
+
+def downsample_waypoints(
+        df_flight_waypoints: pd.DataFrame, *, time_resolution: int = 10, time_var: str = "timestamp"
+) -> pd.DataFrame:
+    """
+    Downsample flight waypoints to a specified time resolution
+
+    Parameters
+    ----------
+    df_flight_waypoints: pd.DataFrame
+        Raw flight waypoints and metadata
+    time_resolution: int
+        Downsampled time resolution, [:math:`s`]
+    time_var: str
+        Time variable in the "df_flight_waypoints" DataFrame
+
+    Returns
+    -------
+    pd.DataFrame
+        Downsampled flight waypoints
+    """
+    df_flight_waypoints.index = df_flight_waypoints[time_var]
+    df_resampled = df_flight_waypoints.resample(f"{time_resolution}s").first()
+    df_resampled = df_resampled[df_resampled["longitude"].notna()].copy()
+    df_resampled.reset_index(inplace=True, drop=True)
+    return df_resampled
+
+
+def separate_unique_flights_from_waypoints(df_waypoints: pd.DataFrame, *, columns: list) -> list[pd.DataFrame]:
+    """
+    Separate unique flights from the waypoints, so each subset has the same metadata.
+
+    Parameters
+    ----------
+    df_waypoints: pd.DataFrame
+        Unsorted waypoints of multiple flights
+    columns: str
+        Metadata variables to check, by ascending order
+
+    Returns
+    -------
+    list[pd.DataFrame]
+        Set of flight waypoints with the same metadata.
+    """
+    if np.any(~pd.Series(columns).isin(df_waypoints.columns)):
+        raise KeyError(f"DataFrame does not contain all of the required columns listed in the inputs.")
+
+    flights = list(df_waypoints.groupby(columns))
+    return [flight[1] for flight in flights]
--- pycontrails/datalib/ads_b/spire.py
+++ pycontrails/datalib/ads_b/spire.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-# from pycontrails.datalib.ads_b.common import unique_flight_waypoints
+import pycontrails.datalib.ads_b.common as adsb
 
 WAYPOINTS_PREFIX = "F:/Spire/data-hi-res"
+COLUMNS_REQ = [
+    'icao_address', 'timestamp', 'latitude', 'longitude', 'altitude_baro', 'heading', 'speed',  'on_ground',
+    'callsign', 'tail_number', 'collection_type', 'aircraft_type_icao', 'aircraft_type_name',
+    'airline_iata', 'airline_name', 'departure_utc_offset', 'departure_scheduled_time'
+]
 # TODO: log?
 
 
@@ -16,34 +21,52 @@ def load_waypoints(t_start: pd.Timestamp, t_end: pd.Timestamp, *, dt: int = "300
         rpath = f"{WAYPOINTS_PREFIX}/{t_str}.pq"
 
         try:
-            df_wypts_t = pd.read_parquet(rpath)
+            df_waypoints_t = pd.read_parquet(rpath, columns=COLUMNS_REQ)
         except FileNotFoundError:
             continue
         else:
-            df_waypoints.append(df_wypts_t)
+            df_waypoints.append(df_waypoints_t)
 
     df_waypoints = pd.concat(df_waypoints)
+    df_waypoints["timestamp"] = pd.to_datetime(df_waypoints["timestamp"])
+    df_waypoints.sort_values(by=["timestamp"], ascending=True, inplace=True)
+
+    #: Remove waypoints without altitude data
+    df_waypoints = df_waypoints[df_waypoints["altitude_baro"].notna()]
 
     #: Remove waypoints without aircraft type and tail number metadata
-    #: Satellites waypoints should contain these information
-    tn = df_waypoints["tail_number"].unique().astype(str)
+    #: Satellites waypoints should contain these data
+    tn = df_waypoints["tail_number"].unique()
     tn = tn[(tn != "None") & (tn != "VARIOUS")]
     df_waypoints = df_waypoints[df_waypoints["tail_number"].isin(tn)]
 
-    atyps = df_waypoints["aircraft_type_icao"].unique().astype(str)
+    atyps = df_waypoints["aircraft_type_icao"].unique()
     atyps = atyps[(atyps != "N/A") & (atyps != "None")]
     df_waypoints = df_waypoints[df_waypoints["aircraft_type_icao"].isin(atyps)]
+    df_waypoints.reset_index(inplace=True, drop=True)
     return df_waypoints
 
 
-def unique_flights_in_icao_address(df_flt_wypts: pd.DataFrame):
-    flights = adsb.unique_flight_waypoints(df_flt_wypts, column="tail_number")
-    # TODO: Separate by aircraft type
-    # TODO: Separate by callsign, backfill satellites
+def identify_unique_flights(df_flight_waypoints: pd.DataFrame):
+    # For each subset of waypoints with the same ICAO address, identify unique flights
+    df_flight_waypoints = adsb.downsample_waypoints(df_flight_waypoints, time_resolution=10, time_var="timestamp")
+    df_flight_waypoints = _fill_missing_callsign_for_satellite_waypoints(df_flight_waypoints)
+    flights = adsb.separate_unique_flights_from_waypoints(
+        df_flight_waypoints, columns=["tail_number", "aircraft_type_icao", "callsign"]
+    )
 
-
-    # TODO: For each ICAO address, identify unique flights
     return
+
+
+def _fill_missing_callsign_for_satellite_waypoints(df_flight_waypoints: pd.DataFrame):
+    if np.any(df_flight_waypoints["collection_type"] == "satellite"):
+        is_missing = df_flight_waypoints["callsign"].isna() & (df_flight_waypoints["collection_type"] == "satellite")
+
+        if np.any(is_missing):
+            df_flight_waypoints["callsign"] = df_flight_waypoints["callsign"].fillna(method="ffill")
+            df_flight_waypoints["callsign"] = df_flight_waypoints["callsign"].fillna(method="bfill")
+
+    return df_flight_waypoints
 
 
 t_start = pd.to_datetime("2022-02-01 00:00:00")
@@ -53,22 +76,7 @@ df_wypts = load_waypoints(t_start, t_end)
 
 # Separate flights by ICAO address
 flts_icao_address = list(df_wypts.groupby("icao_address"))
-
+df_flt_wypts = flts_icao_address[101][1]
 print(" ")
-
-
-"""
-Omitted codes
-
-# TODO: Filter valid waypoints
-    # Identify unique flights by callsign
-    callsigns = df_waypoints["callsign"].unique().astype(str)
-    is_valid = (np.char.str_len(callsigns) > 1) & (~np.char.isspace(callsigns))
-    callsigns = callsigns[is_valid]
-
-for icao_add, df_flt_wypts in tqdm(flts_icao_address):
-
-    print(" ")
-"""
 
 
