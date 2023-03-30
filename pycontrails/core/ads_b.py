2a86e5b9c31c8665e16892e846bf021624fb22c2
rogerteoh <rt415@ic.ac.uk>
2023-03-30 16:30:52 +0100

    Remove noise in cruise altitude

--- pycontrails/core/ads_b.py
+++ pycontrails/core/ads_b.py
@@ -55,3 +55,36 @@ def separate_unique_flights_from_waypoints(df_waypoints: pd.DataFrame, *, column
 
     flights = list(df_waypoints.groupby(columns))
     return [flight[1] for flight in flights]
+
+
+def remove_noise_in_cruise_altitude(
+        altitude_ft: np.ndarray, *,
+        noise_threshold_ft: float = 25,
+        threshold_altitude_ft: float = 10000
+) -> np.ndarray:
+    """
+    Remove noise in cruise altitude by rounding up/down to the nearest flight level.
+
+    Parameters
+    ----------
+    altitude_ft: np.ndarray
+        Barometric altitude, [:math:`ft`]
+    noise_threshold_ft: float
+        Altitude difference threshold to identify noise, [:math:`ft`]
+        Barometric altitude from ADS-B telemetry is reported at increments of 25 feet.
+    threshold_altitude_ft: float
+        Altitude will be checked and corrected above this threshold, [:math:`ft`]
+        Currently set to 10,000 feet.
+
+    Returns
+    -------
+    np.ndarray
+        Barometric altitude with noise removed, [:math:`ft`]
+    """
+    d_alt_ft = np.diff(altitude_ft, prepend=np.nan)
+    is_noise = (np.abs(d_alt_ft) <= noise_threshold_ft) & (altitude_ft > threshold_altitude_ft)
+
+    # Round to the nearest flight level
+    altitude_rounded = np.round(altitude_ft / 1000) * 1000
+    altitude_ft[is_noise] = altitude_rounded[is_noise]
+    return altitude_ft
