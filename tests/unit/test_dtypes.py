ca587ccd604c2bf3b90a25e32580ac7d7c7642e3
Zeb Engberg <zebengberg@gmail.com>
2023-04-06 12:06:33 -0600

    update dtypes tests for breaking changes in interpolation

--- tests/unit/test_dtypes.py
+++ tests/unit/test_dtypes.py
@@ -18,83 +18,46 @@ from pycontrails.physics import units
 from tests import BADA3_PATH, BADA4_PATH, BADA_AVAILABLE
 
 
-def ds_to_float32(ds: xr.Dataset):
-    """Convert spatial dimensions to float32."""
-    ds["longitude"] = ds["longitude"].astype("float32")
-    ds["latitude"] = ds["latitude"].astype("float32")
-    ds["level"] = ds["level"].astype("float32")
-    return ds.reset_coords(drop=True)
-
-
-def confirm_float32(met: MetDataset):
-    """Confirm all data arrays are float32."""
-    assert met.data["longitude"].dtype == "float32"
-    assert met.data["latitude"].dtype == "float32"
-    assert met.data["level"].dtype == "float32"
-    if met.data["level"].size > 1:
-        assert met.data["air_pressure"].dtype == "float32"
-        assert met.data["altitude"].dtype == "float32"
-    assert met.data["time"].dtype == "datetime64[ns]"
-    for v in met:
-        assert met[v].data.dtype == "float32"
-
-
 @pytest.fixture
-def met32(met_ecmwf_pl_path: str):
-    """Create a MetDataset with float32 precision spatial coordinates."""
+def met(met_ecmwf_pl_path: str) -> MetDataset:
+    """Create a MetDataset for testing."""
     ds1 = xr.open_dataset(met_ecmwf_pl_path)
-    ds1 = ds_to_float32(ds1)
 
     # shift time and concatenate to create a new dataset
     ds2 = ds1.copy()
     ds2["time"] = ds2["time"] + np.timedelta64(1, "h")
     ds = xr.concat([ds1, ds2], dim="time")
+    for v in ds.data_vars:
+        assert ds[v].dtype == "float32"
 
     met = MetDataset(ds)
-    confirm_float32(met)
     assert met.shape == (15, 8, 3, 4)
     return met
 
 
-@pytest.mark.parametrize("method", ["linear", "nearest"])
-def test_interpolation_float32_out(method: str, met32: MetDataset):
-    """Ensure interpolation output is float32 if all inputs have float32 precision."""
-    mda = met32["t"]
+@pytest.fixture(params=["float32", "float64"])
+def mda(met: MetDataset, request) -> MetDataArray:
+    """Create a MetDataArray for testing."""
+    mda = met["t"]
+    mda.data = mda.data.astype(request.param)
+    return mda
 
-    t0 = met32.data.time.values[0] + np.timedelta64(30, "m")
+
+@pytest.mark.parametrize("vector_dtype", ["float32", "float64", "int32", "int64"])
+@pytest.mark.parametrize("method", ["linear", "nearest"])
+def test_interpolation_out_dtype(mda: MetDataArray, method: str, vector_dtype: str):
+    """Ensure interpolation output dtype is consistent with grid dtype."""
+
+    t0 = mda.data.time.values[0] + np.timedelta64(30, "m")
     vector = GeoVectorDataset(
-        longitude=np.array([1, 2, 3], dtype="float32"),
-        latitude=np.array([1, 2, 3], dtype="float32"),
-        level=np.array([251, 252, 253], dtype="float32"),
+        longitude=np.array([1, 2, 3], dtype=vector_dtype),
+        latitude=np.array([1, 2, 3], dtype=vector_dtype),
+        level=np.array([251, 252, 253], dtype=vector_dtype),
         time=[t0, t0, t0],
     )
     out = vector.intersect_met(mda, bounds_error=True, method=method)
     assert out.size == 3
-    assert out.dtype == "float32"
-
-    # Linear interpolation is somewhat special in that it issues explicit distances
-    # between coordinates. Nearest interpolation does not.
-    mda.data["longitude"] = mda.data["longitude"].astype("float64")
-    out = vector.intersect_met(mda, bounds_error=True, method=method)
-    if method == "linear":
-        # If any met coordinate is float64 and method=linear, the output will be float64
-        assert out.dtype == "float64"
-    else:
-        assert out.dtype == "float32"
-    mda.data["longitude"] = mda.data["longitude"].astype("float32")
-
-    vector.update(longitude=vector["longitude"].astype("float64"))
-    out = vector.intersect_met(mda, bounds_error=True, method=method)
-    if method == "linear":
-        # If any vector coordinate is float64 and method=linear, output will be float64
-        assert out.dtype == "float64"
-    else:
-        assert out.dtype == "float32"
-
-    # In either case, if the met variable has float64 coordinates, the output will be float64
-    mda.data = mda.data.astype("float64")
-    out = vector.intersect_met(mda, bounds_error=True, method=method)
-    assert out.dtype == "float64"
+    assert out.dtype == mda.data.dtype
 
 
 def test_unit_conversion():
@@ -109,36 +72,27 @@ def test_unit_conversion():
     assert level_ft.dtype == "float32"
 
 
-@pytest.fixture(scope="module")
-def issr_sac_met32(met_issr: MetDataset):
-    """Create a MetDataset with float32 precision for ISSR."""
-    ds = met_issr.data.copy()
-    ds = ds_to_float32(ds)
-
-    # Check it
-    met = MetDataset(ds)
-    confirm_float32(met)
-    return met
-
-
 @pytest.mark.parametrize("model_class", [ISSR, SAC])
 @pytest.mark.parametrize("method", ["linear", "nearest"])
-def test_issr_sac_grid_output(issr_sac_met32: MetDataset, model_class: type, method: str):
+def test_issr_sac_grid_output(met_issr: MetDataset, model_class: type, method: str):
     """Confirm ISSR and SAC gridded output is float32 when met input is float32."""
+
+    assert all(v == "float32" for v in met_issr.data.dtypes.values())
+
     model = model_class(
-        issr_sac_met32,
+        met_issr,
         interpolation_method=method,
         humidity_scaling=ConstantHumidityScaling(),
     )
     out = model.eval()
     assert isinstance(out, MetDataArray)
     assert out.data.dtype == "float32"
-    assert out.shape == issr_sac_met32.shape
+    assert out.shape == met_issr.shape
 
 
 @pytest.fixture(scope="module")
-def random_vector(issr_sac_met32: MetDataset):
-    """Create a random vector with coordinates mostly matching `issr_sac_met32`."""
+def random_vector(met_issr: MetDataset):
+    """Create a random vector with coordinates mostly matching `met_issr`."""
     # Build random vector
     # Purposely going out of bounds from met
     rng = np.random.default_rng(22777)
@@ -146,7 +100,7 @@ def random_vector(issr_sac_met32: MetDataset):
     latitude = rng.uniform(-90, 90, 1000).astype("float32")
     level = rng.uniform(200, 300, 1000).astype("float32")
 
-    t0 = issr_sac_met32.data.time.values[0]
+    t0 = met_issr.data.time.values[0]
     time = t0 + rng.integers(0, 60, 1000) * np.timedelta64(1, "m")
 
     return GeoVectorDataset(
@@ -161,7 +115,7 @@ def random_vector(issr_sac_met32: MetDataset):
 @pytest.mark.parametrize("model_class", [ISSR, SAC])
 @pytest.mark.parametrize("method", ["linear", "nearest"])
 def test_issr_sac_vector_output(
-    issr_sac_met32: MetDataset,
+    met_issr: MetDataset,
     model_class: type,
     method: str,
     random_vector: GeoVectorDataset,
@@ -169,7 +123,7 @@ def test_issr_sac_vector_output(
     """Confirm ISSR and SAC vector output is float32 when source and met input is float32."""
     # Run the model
     model = model_class(
-        issr_sac_met32,
+        met_issr,
         interpolation_method=method,
         humidity_scaling=ConstantHumidityScaling(),
     )
@@ -181,34 +135,17 @@ def test_issr_sac_vector_output(
     assert out["specific_humidity"].dtype == "float32"
 
     # Confirm interpolation behaves as expected
-    in_bounds = random_vector.coords_intersect_met(issr_sac_met32)
+    in_bounds = random_vector.coords_intersect_met(met_issr)
     interp_non_nan = np.isfinite(out["air_temperature"])
     np.testing.assert_array_equal(in_bounds, interp_non_nan)
-
-
-@pytest.fixture
-def cocip_met_rad(met_cocip1: MetDataset, rad_cocip1: MetDataset) -> tuple[MetDataset, MetDataset]:
-    """Convert cocip met and rad input to float32."""
-    ds_met = met_cocip1.data
-    ds_met = ds_to_float32(ds_met)
-
-    ds_rad = rad_cocip1.data
-    ds_rad = ds_to_float32(ds_rad)
-
-    met = MetDataset(ds_met)
-    rad = MetDataset(ds_rad)
-
-    confirm_float32(met)
-    confirm_float32(rad)
-
-    return met, rad
 
 
 @pytest.mark.skipif(not BADA4_PATH.is_dir(), reason="BADA4 not available")
 @pytest.mark.parametrize("drop_aircraft_performance", [False, True])
 @pytest.mark.filterwarnings("ignore::RuntimeWarning")
 def test_cocip(
-    cocip_met_rad,
+    met_cocip1: MetDataset,
+    rad_cocip1: MetDataset,
     flight_cocip1: Flight,
     drop_aircraft_performance: bool,
     bada_model: AircraftPerformance,
@@ -227,7 +164,8 @@ def test_cocip(
 
     # Run the model
     cocip = Cocip(
-        *cocip_met_rad,
+        met=met_cocip1,
+        rad=rad_cocip1,
         process_emissions=drop_aircraft_performance,
         humidity_scaling=ConstantHumidityScaling(),
         aircraft_performance=bada_model,
@@ -245,15 +183,16 @@ def test_cocip(
     ]
     for vector in vectors:
         for k in vector:
-            if k not in exclude:
-                arr = vector[k]
-                assert np.can_cast(arr.dtype, "float32"), k
+            if k in exclude:
+                continue
+            arr = vector[k]
+            assert np.can_cast(arr.dtype, "float32"), k
 
 
 @pytest.mark.skipif(not BADA_AVAILABLE, reason="BADA not available")
 @pytest.mark.filterwarnings("ignore::RuntimeWarning")
 @pytest.mark.parametrize("bada_priority", [3, 4])
-def test_cocip_grid(cocip_met_rad: tuple[MetDataset, MetDataset], bada_priority: int):
+def test_cocip_grid(met_cocip1: MetDataset, rad_cocip1: MetDataset, bada_priority: int):
     """Confirm `CocipGrid` maintains float32 precision."""
     # Keep max_age small to avoid blowing out of bounds
     # And keep dt_integration small to make the evolution interesting
@@ -267,7 +206,7 @@ def test_cocip_grid(cocip_met_rad: tuple[MetDataset, MetDataset], bada_priority:
         params["bada3_path"] = BADA3_PATH
     elif bada_priority == 4:
         params["bada4_path"] = BADA4_PATH
-    cg = CocipGrid(*cocip_met_rad, params=params)
+    cg = CocipGrid(met=met_cocip1, rad=rad_cocip1, params=params)
 
     # Create float32 source
     source = cg.create_source(
@@ -276,10 +215,6 @@ def test_cocip_grid(cocip_met_rad: tuple[MetDataset, MetDataset], bada_priority:
         longitude=cg.met.data.longitude.values,
         latitude=cg.met.data.latitude.values,
     )
-    ds = source.data
-    ds = ds_to_float32(ds)
-    source = MetDataset(ds)
-    confirm_float32(source)
     assert source.shape == (16, 8, 1, 1)
 
     out = cg.eval(source)
@@ -288,11 +223,8 @@ def test_cocip_grid(cocip_met_rad: tuple[MetDataset, MetDataset], bada_priority:
     assert out.attrs["max_age"] == "2 hours"
     assert out.attrs["dt_integration"] == "10 minutes"
     assert out.attrs["pycontrails_version"].startswith("0.")
-    assert out.data["longitude"].dtype == "float32"
-    assert out.data["latitude"].dtype == "float32"
-    assert out.data["level"].dtype == "float32"
 
     # Not much persistence, but we can pin them
     # And importantly, they are the same with BADA3 or BADA4
-    (nonzero,) = out["ef_per_m"].data.values.ravel().nonzero()
+    nonzero = np.flatnonzero(out["ef_per_m"].data.values.ravel())
     np.testing.assert_array_equal(nonzero, [38, 46, 52, 53, 54, 61, 62, 70])
