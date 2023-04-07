41a19482f4edcb4bad8f50a5572bdd2a87d2e88d
Zeb Engberg <zebengberg@gmail.com>
2023-04-07 12:45:25 -0600

    increase test tolerance for architecture differences

--- tests/unit/test_cocip_grid.py
+++ tests/unit/test_cocip_grid.py
@@ -473,7 +473,7 @@ def test_grid_results(grid_results: MetDataset) -> None:
     # This test is HARD to maintain. Delete if it gets too annoying.
     point = grid_results.data.isel(longitude=14, latitude=19, level=2, time=2)
     assert point["contrail_age"].item() == 1.5
-    assert point["ef_per_m"].item() == pytest.approx(46576688, abs=1)
+    assert point["ef_per_m"].item() == pytest.approx(46576688, abs=10)
 
 
 @pytest.fixture
@@ -501,18 +501,18 @@ def test_grid_results_segment_free(
     assert out1.shape == out2.shape
     assert out1.data.data_vars.keys() == out2.data.data_vars.keys()
 
-    assert np.count_nonzero(out1["ef_per_m"].values) == 605
-    assert np.count_nonzero(out2["ef_per_m"].values) == 621
+    assert np.count_nonzero(out1["ef_per_m"].values) == pytest.approx(605, abs=1)
+    assert np.count_nonzero(out2["ef_per_m"].values) == pytest.approx(621, abs=1)
 
     # Pin some values
     da1 = out1["ef_per_m"].data
     filt1 = da1 > 0
-    assert da1.where(filt1).mean().item() == pytest.approx(39581584, abs=1)
+    assert da1.where(filt1).mean().item() == pytest.approx(39581584, abs=10)
 
     # In segment-free mode (generally and here), the mean nonzero EF is slightly lower
     da2 = out2["ef_per_m"].data
     filt2 = da2 > 0
-    assert da2.where(filt2).mean().item() == pytest.approx(32914018, abs=1)
+    assert da2.where(filt2).mean().item() == pytest.approx(32914018, abs=10)
 
     # For this example, we can say something about the distribution of EFs
     filt = filt1 & filt2
@@ -585,8 +585,8 @@ def test_geovector_source(syn_fl: SyntheticFlight, instance_params: dict[str, An
     assert np.all(out["ef_per_m"][~persistent] == 0)
 
     # Pin the mean EF
-    assert out["ef_per_m"].mean().item() == pytest.approx(875854, abs=1)
-    assert out["ef_per_m"][persistent].mean().item() == pytest.approx(31725403, abs=1)
+    assert out["ef_per_m"].mean().item() == pytest.approx(875854, abs=10)
+    assert out["ef_per_m"][persistent].mean().item() == pytest.approx(31725403, abs=10)
 
 
 @pytest.mark.filterwarnings("ignore:invalid value encountered in remainder")
