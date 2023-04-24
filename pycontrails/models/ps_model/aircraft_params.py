7de704fb143130dd76361530280ec18317961eb8
rogerteoh <rt415@ic.ac.uk>
2023-04-24 16:30:40 +0100

    Extract aircraft-engine parameters - Phase 1

--- pycontrails/models/ps_model/aircraft_params.py
+++ pycontrails/models/ps_model/aircraft_params.py
@@ -22,16 +22,10 @@ class AircraftEngineParams:
     (3) winglets            Does the aircraft type contain winglets? (True/False)
     (4) wing_surface_area   Reference wing surface area, [:math:`m^{2}`]
     (5) delta_2             Induced drag wing-fuselage interference factor
-    (6) cos_sweep           Cosine of wing sweep angle measured at the 1/4 chord line
+    (6) cos_sweep           cosine of wing sweep angle measured at the 1/4 chord line
     (7) wing_aspect_ratio   Wing aspect ratio, wing_span**2 / wing_surface_area
     (8) psi_0               Aircraft geometry drag parameter
-    (9) x_ref               ???
-    (10) wing_constant      ???
-    (11) j_1                Wave drag parameter 1
-    (12) j_2                Wave drag parameter 2
-
     """
-    # TODO: Documentation incomplete
     def __init__(self, df_aircraft_engine: pd.Series) -> None:
         self.aircraft_type: str = df_aircraft_engine["Type"]
         self.manufacturer: str = df_aircraft_engine["Manufacturer"]
@@ -43,10 +37,6 @@ class AircraftEngineParams:
         self.cos_sweep: float = df_aircraft_engine["cos_sweep"]
         self.wing_aspect_ratio: float = df_aircraft_engine["AR"]
         self.psi_0: float = df_aircraft_engine["psi_0"]
-        self.x_ref: float = df_aircraft_engine["X_o"]
-        self.wing_constant: float = df_aircraft_engine["wing constant"]
-        self.j_1: float = df_aircraft_engine["j_1"]
-        self.j_2: float = df_aircraft_engine["j_2"]
         # TODO: Incomplete
 
 
