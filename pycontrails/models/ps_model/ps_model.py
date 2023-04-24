1f31a008fa04d25dab0555fb83561fceaa2a6f4a
rogerteoh <rt415@ic.ac.uk>
2023-04-24 10:11:48 +0100

    Static files and skeleton structure

---
+++ pycontrails/models/ps_model/ps_model.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+class PSModel:
+    def __init__(self):
+        self.aircraft_params = None
+        # TODO: Load aircraft parameters
+
+    def calculate_aircraft_performance(self):
+        # TODO: Calculate atmospheric quantities
+        # TODO: Calculate trajectory parameters
+        # TODO: Calculate aircraft performance parameters
+        # TODO: Calculate engine parameters
+        # TODO: Calculate fuel consumption
+        return
+
