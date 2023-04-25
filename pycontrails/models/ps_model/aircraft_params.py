f20607a60a5902edf920e24e30c727b13d675737
rogerteoh <rt415@ic.ac.uk>
2023-04-25 18:05:33 +0100

    Incorporate Ian comments

--- pycontrails/models/ps_model/aircraft_params.py
+++ pycontrails/models/ps_model/aircraft_params.py
@@ -23,36 +23,38 @@ class AircraftEngineParams:
     (4) wing_surface_area   Reference wing surface area, [:math:`m^{2}`]
     (5) wing_aspect_ratio   Wing aspect ratio, wing_span**2 / wing_surface_area
     (6) wing_span           Wing span, [:math:`m`]
-    (7) wing_constant       ???
+    (7) wing_constant       A constant used in the wave drag model, capturing the aerofoil technology factor and wing
+                            geometry
     (8) delta_2             Induced drag wing-fuselage interference factor
     (9) cos_sweep           Cosine of wing sweep angle measured at the 1/4 chord line
     (10) psi_0              Aircraft geometry drag parameter
-    (11) x_ref              ???
+    (11) x_ref              Threshold condition where the terminating shockwave moves onto the rear part of the wing
     (12) j_1                Wave drag parameter 1
     (13) j_2                Wave drag parameter 2
+    (14) j_3                Wave drag parameter 3
 
     -------------------------------------
     ENGINE PARAMETERS
     -------------------------------------
-    (14) ff_idle_sls        Fuel mass flow rate under engine idle and sea level static conditions, summed over
+    (15) ff_idle_sls        Fuel mass flow rate under engine idle and sea level static conditions, summed over
                             all engines, [:math:`kg s^{-1}`]
-    (15) m_des              Design optimum Mach number where the fuel mass flow rate is at a minimum.
-    (16) c_t_des            Design optimum engine thrust coefficient where the fuel mass flow rate is at a minimum.
-    (17) eta_1              Multiplier for maximum overall propulsion efficiency model
-    (18) eta_2              Exponent for maximum overall propulsion efficiency model
+    (16) m_des              Design optimum Mach number where the fuel mass flow rate is at a minimum.
+    (17) c_t_des            Design optimum engine thrust coefficient where the fuel mass flow rate is at a minimum.
+    (18) eta_1              Multiplier for maximum overall propulsion efficiency model
+    (19) eta_2              Exponent for maximum overall propulsion efficiency model
 
     -------------------------------------
     OPERATIONAL PARAMETERS
     -------------------------------------
-    (19) amass_mtow         Aircraft maximum take-off weight, [:math:`kg`]
-    (20) amass_mlw          Aircraft maximum landing weight, [:math:`kg`]
-    (21) amass_mzfw         Aircraft maximum zero fuel weight, [:math:`kg`]
-    (22) amass_oew          Aircraft operating empty weight, [:math:`kg`]
-    (23) amass_mpl          Aircraft maximum payload, [:math:`kg`]
-    (24) max_altitude_ft    Maximum altitude, [:math:`ft`]
-    (25) max_mach_num       Maximum operational Mach number
-    (26) wingspan           Aircraft wingspan, [:math:`m`]
-    (27) fuselage_width     Aircraft fuselage width, [:math:`m`]
+    (20) amass_mtow         Aircraft maximum take-off weight, [:math:`kg`]
+    (21) amass_mlw          Aircraft maximum landing weight, [:math:`kg`]
+    (22) amass_mzfw         Aircraft maximum zero fuel weight, [:math:`kg`]
+    (23) amass_oew          Aircraft operating empty weight, [:math:`kg`]
+    (24) amass_mpl          Aircraft maximum payload, [:math:`kg`]
+    (25) max_altitude_ft    Maximum altitude, [:math:`ft`]
+    (26) max_mach_num       Maximum operational Mach number
+    (27) wingspan           Aircraft wingspan, [:math:`m`]
+    (28) fuselage_width     Aircraft fuselage width, [:math:`m`]
     """
     def __init__(self, df_aircraft_engine: pd.Series) -> None:
         self.manufacturer: str = df_aircraft_engine["Manufacturer"]
@@ -70,6 +72,7 @@ class AircraftEngineParams:
         self.x_ref: float = df_aircraft_engine["X_o"]
         self.j_1: float = df_aircraft_engine["j_1"]
         self.j_2: float = df_aircraft_engine["j_2"]
+        self.j_3: float = 70        # Constant for all aircraft-engine types for now, but may vary in the future
 
         # Engine parameters
         self.ff_idle_sls: float = df_aircraft_engine["nominal((mf)FI)SLS (kg/s)"]
