5b67526e087ab05621356acd0b28cf977092d8ef
Zeb Engberg <zebengberg@gmail.com>
2023-05-05 21:14:41 +0000

    define __all__ in else clause

--- pycontrails/ext/cirium/__init__.py
+++ pycontrails/ext/cirium/__init__.py
@@ -5,10 +5,10 @@ from __future__ import annotations
 try:
     from pycontrails_cirium import Cirium
 
-    __all__ = ["Cirium"]
-
 except ImportError as e:
     raise ImportError(
         "Failed to import `pycontrails-cirium` extension. Install with `pip install"
         ' "pycontrails-cirium @ git+ssh://git@github.com/contrailcirrus/pycontrails-cirium.git"`'
     ) from e
+else:
+    __all__ = ["Cirium"]
