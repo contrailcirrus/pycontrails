f67df51da83efe9a7d98331ac77e01f12bab7112
Zeb Engberg <zebengberg@gmail.com>
2023-05-09 17:38:34 +0000

    fix doctests

--- pycontrails/models/cocip/cocip_uncertainty.py
+++ pycontrails/models/cocip/cocip_uncertainty.py
@@ -7,6 +7,7 @@ from dataclasses import asdict, dataclass
 from typing import Any, ClassVar
 
 import numpy as np
+import numpy.typing as npt
 from scipy import stats
 from scipy.stats.distributions import rv_frozen
 
@@ -32,7 +33,7 @@ class habit_dirichlet(rv_frozen):
     def __init__(self, C: float = 96.0):
         self.C = C
 
-    def rvs(self, *args: Any, **kwds: Any) -> np.ndarray:
+    def rvs(self, *args: Any, **kwds: Any) -> npt.NDArray[np.float32]:
         """Generate sample set of habit distributions.
 
         Sampled using dirichlet distribution.
@@ -46,7 +47,7 @@ class habit_dirichlet(rv_frozen):
 
         Returns
         -------
-        np.ndarray
+        npt.NDArray[np.float32]
             Sampled habit weight distribution with the same shape
             as :attr:`CocipParams().habit_distributions`
         """
@@ -86,17 +87,19 @@ class CocipUncertaintyParams(CocipParams):
     >>> import scipy.stats
     >>> from pycontrails.models.cocip import CocipUncertaintyParams
 
-    # Override the `rhi_adj` field from `CocipParams` with a value in [0.97, 0.99]
-    >>> distr = scipy.stats.uniform(loc=0.97, scale=0.02)
-    >>> params = CocipUncertaintyParams(seed=123, rhi_adj_uncertainty=distr)
-    >>> params.rhi_adj
-    0.98364703
+    >>> # Override the 'initial_wake_vortex_depth' field from
+    >>> # CocipParams with a uniform value in [0.4, 0.6]
+    >>> distr = scipy.stats.uniform(loc=0.4, scale=0.2)
+    >>> params = CocipUncertaintyParams(seed=123, initial_wake_vortex_depth_uncertainty=distr)
+    >>> params.initial_wake_vortex_depth
+    0.41076420
 
-    # Once seeded, calling the class again gives a new value
-    >>> params = CocipUncertaintyParams(rhi_adj_uncertainty=distr)
-    >>> params.rhi_adj
-    0.98310570
+    >>> # Once seeded, calling the class again gives a new value
+    >>> params = CocipUncertaintyParams(initial_wake_vortex_depth=distr)
+    >>> params.initial_wake_vortex_depth
+    0.43526372
 
+    >>> # To retain the default value, set the uncertainty to None
     >>> params = CocipUncertaintyParams(rf_lw_enhancement_factor_uncertainty=None)
     >>> params.rf_lw_enhancement_factor
     1.0
@@ -209,7 +212,7 @@ class CocipUncertaintyParams(CocipParams):
 
         return out
 
-    def rvs(self, size: None | int = None) -> dict[str, float | np.ndarray]:
+    def rvs(self, size: None | int = None) -> dict[str, float | npt.NDArray[np.float_]]:
         """Call each distribution's `rvs` method to generate random parameters.
 
         Seed calls to `rvs` with class variable `rng`.
@@ -221,7 +224,7 @@ class CocipUncertaintyParams(CocipParams):
 
         Returns
         -------
-        dict[str, float | np.ndarray]
+        dict[str, float | npt.NDArray[np.float_]]
             Dictionary of random parameters. Dictionary keys consists of names of parameters in
             `CocipParams` to be overridden by random value.
 
@@ -232,26 +235,24 @@ class CocipUncertaintyParams(CocipParams):
         >>> params = CocipUncertaintyParams(seed=456)
         >>> pprint(params.rvs())
         {'habit_distributions': array([[0.0000000e+00, 0.0000000e+00, 0.0000000e+00, 0.0000000e+00,
-               0.0000000e+00, 0.0000000e+00, 1.0000000e+00, 0.0000000e+00],
-              [3.6062175e-04, 2.9157758e-01, 6.5901526e-04, 1.3415117e-03,
-               5.0444747e-03, 2.6956137e-02, 6.7330402e-01, 7.5663364e-04],
-              [2.3834489e-03, 3.6896354e-01, 1.3977488e-03, 4.7844747e-04,
-               3.0474603e-01, 4.8765671e-03, 3.1715181e-01, 2.4031256e-06],
-              [4.0944587e-03, 5.1772928e-01, 4.8383059e-05, 1.3166944e-03,
-               1.0002965e-01, 3.7468293e-01, 3.9641498e-04, 1.7021718e-03],
-              [1.5192166e-05, 3.8423434e-01, 5.0536448e-01, 9.4547205e-02,
-               1.1378267e-03, 3.3812344e-03, 4.6305601e-03, 6.6891452e-03],
-              [1.1620003e-03, 2.3161094e-03, 1.6705607e-04, 2.0091899e-02,
-               9.7001791e-01, 1.5312615e-03, 3.9347797e-03, 7.7895907e-04]],
-             dtype=float32),
-        'initial_wake_vortex_depth': 0.4752143043559352,
-        'nvpm_ei_n_enhancement_factor': 1.0094726146454185,
-        'rf_lw_enhancement_factor': 0.9718354129386728,
-        'rf_sw_enhancement_factor': 1.016450567639041,
-        'rhi_adj': 1.0293074966694602,
-        'rhi_boost_exponent': 1.3308280817638443,
-        'sedimentation_impact_factor': 0.6433086271594898,
-        'wind_shear_enhancement_exponent': 0.31735944665388505}
+                0.0000000e+00, 0.0000000e+00, 1.0000000e+00, 0.0000000e+00],
+               [1.5554131e-02, 2.1363135e-01, 7.7715185e-03, 1.7690966e-02,
+                3.1576434e-03, 3.2992734e-06, 7.4009895e-01, 2.0921326e-03],
+               [3.8193921e-03, 2.1235342e-01, 3.3554080e-04, 5.2846869e-04,
+                3.1945917e-01, 4.8709914e-04, 4.6250960e-01, 5.0730183e-04],
+               [5.7327619e-04, 4.7781631e-01, 4.2596990e-03, 6.7235163e-04,
+                1.4447135e-01, 3.6184600e-01, 1.0150939e-02, 2.1006212e-04],
+               [1.5397545e-02, 4.0522218e-01, 4.2781001e-01, 1.4331797e-01,
+                7.1088417e-04, 9.4511814e-04, 3.3900745e-03, 3.2062260e-03],
+               [7.9063961e-04, 3.0336906e-03, 7.7571563e-04, 2.0577813e-02,
+                9.4205803e-01, 4.3379897e-03, 3.6786550e-03, 2.4747452e-02]],
+              dtype=float32),
+         'initial_wake_vortex_depth': 0.39805019708566847,
+         'nvpm_ei_n_enhancement_factor': 0.9371878437312526,
+         'rf_lw_enhancement_factor': 1.1017491252832377,
+         'rf_sw_enhancement_factor': 0.99721639115012,
+         'sedimentation_impact_factor': 0.5071779847244678,
+         'wind_shear_enhancement_exponent': 0.34100931239701004}
         """
         return {
             param: distr.rvs(size=size, random_state=self.rng)
