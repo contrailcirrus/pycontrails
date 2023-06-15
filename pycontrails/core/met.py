d338a97c9fe842b4fb52be687ebab958d8d9af34
rogerteoh <rt415@ic.ac.uk>
2023-06-15 15:07:34 +0100

    Fix `ensure_vars` bug in MetDataset

--- pycontrails/core/met.py
+++ pycontrails/core/met.py
@@ -855,7 +855,7 @@ class MetDataset(MetBase):
         KeyError
             Raises when dataset does not contain variable in ``vars``
         """
-        if isinstance(vars, MetVariable):
+        if isinstance(vars, (MetVariable, str)):
             vars = [vars]
 
         met_keys: list[str] = []
