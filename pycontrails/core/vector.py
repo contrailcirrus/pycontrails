50f651fa6f145cebcb33c62ae3b2e609549212b5
Zeb Engberg <zebengberg@gmail.com>
2023-07-05 08:13:32 -0600

    fix issues encountered with ruff 0.0.277

--- pycontrails/core/vector.py
+++ pycontrails/core/vector.py
@@ -83,7 +83,7 @@ class VectorDataDict(Dict[str, np.ndarray]):
         Keyword arguments, override values in ``data``
     """
 
-    __slots__ = "_size"
+    __slots__ = ("_size",)
 
     #: Length of the data
     _size: int
