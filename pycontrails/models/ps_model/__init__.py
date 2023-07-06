350bb3163f866527a2dc67a21d0216be8c5c0121
Zeb Engberg <zebengberg@gmail.com>
2023-07-06 16:45:52 +0000

    WIP: prototype nominal PSGrid

--- pycontrails/models/ps_model/__init__.py
+++ pycontrails/models/ps_model/__init__.py
@@ -4,6 +4,13 @@ from pycontrails.models.ps_model.ps_aircraft_params import (
     PSAircraftEngineParams,
     load_aircraft_engine_params,
 )
+from pycontrails.models.ps_model.ps_grid import ps_nominal_grid
 from pycontrails.models.ps_model.ps_model import PSModel, PSModelParams
 
-__all__ = ["PSModel", "PSModelParams", "PSAircraftEngineParams", "load_aircraft_engine_params"]
+__all__ = [
+    "PSModel",
+    "PSModelParams",
+    "PSAircraftEngineParams",
+    "load_aircraft_engine_params",
+    "ps_nominal_grid",
+]
