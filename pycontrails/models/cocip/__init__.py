c989676e180a2105e1821edba5fd5c843be61ed2
Zeb Engberg <zebengberg@gmail.com>
2023-07-18 08:56:02 -0600

    add output functions to cocip.__init__

--- pycontrails/models/cocip/__init__.py
+++ pycontrails/models/cocip/__init__.py
@@ -3,10 +3,24 @@
 from pycontrails.models.cocip.cocip import Cocip
 from pycontrails.models.cocip.cocip_params import CocipFlightParams, CocipParams
 from pycontrails.models.cocip.cocip_uncertainty import CocipUncertaintyParams
+from pycontrails.models.cocip.output_formats import (
+    contrail_flight_summary_statistics,
+    contrails_to_hi_res_grid,
+    flight_waypoint_summary_statistics,
+    longitude_latitude_grid,
+    natural_cirrus_properties_to_hi_res_grid,
+    time_slice_statistics,
+)
 
 __all__ = [
     "Cocip",
     "CocipParams",
     "CocipUncertaintyParams",
     "CocipFlightParams",
+    "contrail_flight_summary_statistics",
+    "contrails_to_hi_res_grid",
+    "flight_waypoint_summary_statistics",
+    "longitude_latitude_grid",
+    "natural_cirrus_properties_to_hi_res_grid",
+    "time_slice_statistics",
 ]
