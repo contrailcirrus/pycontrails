3187aae456500078e543660db8c9eb133a1eef3d
Zeb Engberg <zebengberg@gmail.com>
2023-08-25 15:08:07 +0000

    add habit_dirichlet distribution to the cocip module

--- pycontrails/models/cocip/__init__.py
+++ pycontrails/models/cocip/__init__.py
@@ -2,7 +2,7 @@
 
 from pycontrails.models.cocip.cocip import Cocip
 from pycontrails.models.cocip.cocip_params import CocipFlightParams, CocipParams
-from pycontrails.models.cocip.cocip_uncertainty import CocipUncertaintyParams
+from pycontrails.models.cocip.cocip_uncertainty import CocipUncertaintyParams, habit_dirichlet
 from pycontrails.models.cocip.output_formats import (
     contrail_flight_summary_statistics,
     contrails_to_hi_res_grid,
@@ -20,6 +20,7 @@ __all__ = [
     "contrail_flight_summary_statistics",
     "contrails_to_hi_res_grid",
     "flight_waypoint_summary_statistics",
+    "habit_dirichlet",
     "longitude_latitude_grid",
     "natural_cirrus_properties_to_hi_res_grid",
     "time_slice_statistics",
