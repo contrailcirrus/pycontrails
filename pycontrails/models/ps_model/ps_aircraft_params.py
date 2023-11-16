618e96b0d38d8d6378b016d8c2e2f62e243b6c71
Roger Teoh <roger.teoh15@imperial.ac.uk>
2023-11-16 16:26:18 +0800

    Update PS model aircraft parameters

--- pycontrails/models/ps_model/ps_aircraft_params.py
+++ pycontrails/models/ps_model/ps_aircraft_params.py
@@ -6,11 +6,12 @@ import dataclasses
 import functools
 import pathlib
 from typing import Any, Mapping
+from pycontrails.physics import constants as c
 
 import pandas as pd
 
 #: Path to the Poll-Schumann aircraft parameters CSV file.
-PS_FILE_PATH = pathlib.Path(__file__).parent / "static" / "ps-aircraft-params-20230517.csv"
+PS_FILE_PATH = pathlib.Path(__file__).parent / "static" / "ps-aircraft-params-20231115.csv"
 
 
 @dataclasses.dataclass(frozen=True)
@@ -23,78 +24,69 @@ class PSAircraftEngineParams:
     - ``manufacturer``        Aircraft manufacturer name
     - ``aircraft_type``       Specific aircraft type variant
     - ``n_engine``            Number of engines
-
-    -------------------------------------
-    AIRCRAFT PARAMETERS
-    -------------------------------------
     - ``winglets``            Does the aircraft type contain winglets? (True/False)
-    - ``wing_surface_area``   Reference wing surface area, [:math:`m^{2}`]
-    - ``wing_aspect_ratio``   Wing aspect ratio, ``wing_span**2 / wing_surface_area``
-    - ``wing_span``           Wing span, [:math:`m`]
-    - ``wing_constant``       A constant used in the wave drag model, capturing the
-      aerofoil technology factor and wing geometry
-    - ``delta_2``             Induced drag wing-fuselage interference factor
-    - ``cos_sweep``           Cosine of wing sweep angle measured at the 1/4 chord line
-    - ``psi_0``               Aircraft geometry drag parameter
-    - ``x_ref``               Threshold condition where the terminating shockwave moves
-      onto the rear part of the wing
-    - ``j_1``                 Wave drag parameter 1
-    - ``j_2``                 Wave drag parameter 2
-    - ``j_3``                 Wave drag parameter 3
 
     -------------------------------------
-    ENGINE PARAMETERS
-    -------------------------------------
-    - ``ff_idle_sls``        Fuel mass flow rate under engine idle and sea level
-      static conditions, summed over all engines, [:math:`kg s^{-1}`]
-    - ``ff_max_sls``         Fuel mass flow rate at take-off and sea level static conditions,
-      summed over all engines, [:math:`kg s^{-1}`]
-    - ``f_00``               Maximum thrust force that can be supplied by the engine at
-      sea level static conditions, summed over all engines, [:math:`N`].
-    - ``m_des``              Design optimum Mach number where the fuel mass flow rate
-      is at a minimum.
-    - ``c_t_des``            Design optimum engine thrust coefficient where the fuel mass
-      flow rate is at a minimum.
-    - ``eta_1``              Multiplier for maximum overall propulsion efficiency model
-    - ``eta_2``              Exponent for maximum overall propulsion efficiency model
-
-    -------------------------------------
-    OPERATIONAL PARAMETERS
+    AIRCRAFT MASS PARAMETERS
     -------------------------------------
     - ``amass_mtow``         Aircraft maximum take-off weight, [:math:`kg`]
     - ``amass_mlw``          Aircraft maximum landing weight, [:math:`kg`]
     - ``amass_mzfw``         Aircraft maximum zero fuel weight, [:math:`kg`]
     - ``amass_oew``          Aircraft operating empty weight, [:math:`kg`]
     - ``amass_mpl``          Aircraft maximum payload, [:math:`kg`]
-    - ``max_altitude_ft``    Maximum altitude, [:math:`ft`]
-    - ``max_mach_num``       Maximum operational Mach number
-    - ``fuselage_width``     Aircraft fuselage width, [:math:`m`]
-    """
 
+    -------------------------------------
+    AIRCRAFT GEOMETRY
+    -------------------------------------
+    - ``wing_surface_area``     Reference wing surface area, [:math:`m^{2}`]
+    - ``wing_span``             Wing span, [:math:`m`]
+    - ``fuselage_width``        Aircraft fuselage width, [:math:`m`]
+    - ``delta_2``               Induced drag wing-fuselage interference factor
+    - ``cos_sweep``             Cosine of wing sweep angle measured at the 1/4 chord line
+    - ``wing_aspect_ratio``     Wing aspect ratio, ``wing_span**2 / wing_surface_area``
+
+    -------------------------------------
+    AERODYNAMIC PARAMETERS
+    -------------------------------------
+    - ``psi_0``             Aircraft geometry drag parameter
+    - ``x_ref``             Threshold condition where the terminating shockwave moves onto
+                            the rear part of the wing
+    - ``wing_constant``     A constant used in the wave drag model, capturing the aerofoil
+                            technology factor and wing geometry
+    - ``j_1``               Wave drag parameter 1
+    - ``j_2``               Wave drag parameter 2
+    - ``j_3``               Wave drag parameter 3
+
+    -------------------------------------
+    ENGINE PARAMETERS
+    -------------------------------------
+    - ``f_00``          Maximum thrust force that can be supplied by the engine at sea level static
+                        conditions, summed over all engines, [:math:`N`].
+    - ``ff_max_sls``    Fuel mass flow rate at take-off and sea level static conditions, summed
+                        over all engines, [:math:`kg s^{-1}`]
+    - ``ff_idle_sls``   Fuel mass flow rate under engine idle and sea level static conditions,
+                        summed over all engines, [:math:`kg s^{-1}`]
+    - ``m_des``         Design optimum Mach number where the fuel mass flow rate is at a minimum.
+    - ``c_t_des``       Design optimum engine thrust coefficient where the fuel mass flow rate is
+                        at a minimum.
+    - ``eta_1``         Multiplier for maximum overall propulsion efficiency model
+    - ``eta_2``         Exponent for maximum overall propulsion efficiency model
+    - ``mec``           Constant used to calculate the throttle parameter
+    - ``tec``           Constant used to calculate the throttle parameter
+    - ``tet_max_climb`` Turbine entry temperature at maximum continuous climb rating, [:math:`K`]
+
+    -------------------------------------
+    HEIGHT AND SPEED LIMITS
+    -------------------------------------
+    - ``fl_max``        Maximum flight level
+    - ``max_mach_num``  Maximum operational Mach number
+    - ``p_i_max``       Maximum operational impact pressure, [:math:`Pa`]
+    - ``p_inf_co``      Crossover pressure altitude, [:math:`Pa`]
+    """
     manufacturer: str
     aircraft_type: str
     n_engine: int
-
     winglets: bool
-    wing_surface_area: float
-    wing_aspect_ratio: float
-    wing_span: float
-    wing_constant: float
-    delta_2: float
-    cos_sweep: float
-    psi_0: float
-    x_ref: float
-    j_1: float
-    j_2: float
-    j_3: float
-
-    ff_idle_sls: float
-    ff_max_sls: float
-    f_00: float
-    m_des: float
-    c_t_des: float
-    eta_1: float
-    eta_2: float
 
     amass_mtow: float
     amass_mlw: float
@@ -102,47 +94,77 @@ class PSAircraftEngineParams:
     amass_oew: float
     amass_mpl: float
 
-    max_altitude_ft: float
-    max_mach_num: float
+    wing_surface_area: float
+    wing_span: float
     fuselage_width: float
+    delta_2: float
+    cos_sweep: float
+    wing_aspect_ratio: float
+
+    psi_0: float
+    x_ref: float
+    wing_constant: float
+    j_1: float
+    j_2: float
+    j_3: float
+
+    f_00: float
+    ff_max_sls: float
+    ff_idle_sls: float
+    m_des: float
+    c_t_des: float
+    eta_1: float
+    eta_2: float
+    mec: float
+    tec: float
+    tet_max_climb: float
+
+    fl_max: float
+    max_mach_num: float
+    p_i_max: float
+    p_inf_co: float
 
 
 def _row_to_aircraft_engine_params(tup: Any) -> tuple[str, PSAircraftEngineParams]:
     icao = tup.ICAO
     wing_aspect_ratio = tup.AR
-    wing_surface_area = tup.Sref_m2
     amass_mtow = tup.MTOM_kg
+    p_i_max = impact_pressure_max_operating_limits(tup.MMO)
     params = PSAircraftEngineParams(
         manufacturer=tup.Manufacturer,
         aircraft_type=tup.Type,
         n_engine=tup.n_engine,
         winglets=tup.winglets == "yes",
-        wing_surface_area=wing_surface_area,
-        wing_aspect_ratio=wing_aspect_ratio,
-        wing_span=(wing_aspect_ratio * wing_surface_area) ** 0.5,
-        wing_constant=tup.wing_constant,
-        delta_2=tup.delta_2,
-        cos_sweep=tup.cos_sweep,
-        psi_0=tup.psi_0,
-        x_ref=tup.Xo,
-        j_1=tup.j_1,
-        j_2=tup.j_2,
-        j_3=70.0,  # use constant value for now, may be updated in the future
-        ff_idle_sls=tup.mf_idle_SLS_kg_s,
-        ff_max_sls=tup.mf_max_T_O_SLS_kg_s,
-        f_00=tup.nominal_F00_ISA_kn * 1000.0,
-        m_des=tup.M_des,
-        c_t_des=tup.CT_des,
-        eta_1=tup.eta_1,
-        eta_2=tup.eta_2,
         amass_mtow=amass_mtow,
         amass_mlw=tup.MLM_kg,
         amass_mzfw=tup.MZFM_kg,
         amass_oew=tup.OEM_i_kg,
         amass_mpl=tup.MPM_i_kg,
-        max_altitude_ft=tup.MaxAlt_ft,
+        wing_surface_area=tup.Sref_m2,
+        wing_span=tup.span_m,
+        delta_2=tup.delta_2,
+        cos_sweep=tup.cos_sweep,
+        wing_aspect_ratio=wing_aspect_ratio,
+        psi_0=tup.psi_0,
+        x_ref=tup.Xo,
+        wing_constant=tup.wing_constant,
+        j_1=tup.j_1,
+        j_2=tup.j_2,
+        j_3=70.0,  # use constant value for now, may be updated in the future
+        f_00=tup.nominal_F00_ISA_kn * 1000.0,
+        ff_max_sls=tup.mf_max_T_O_SLS_kg_s,
+        ff_idle_sls=tup.mf_idle_SLS_kg_s,
+        m_des=tup.M_des,
+        c_t_des=tup.CT_des,
+        eta_1=tup.eta_1,
+        eta_2=tup.eta_2,
+        mec=tup.Mec,
+        tec=tup.Tec,
+        tet_max_climb=tup.TET_max_climb_K,
+        fl_max=tup.FL_max,
         max_mach_num=tup.MMO,
-        fuselage_width=tup.bf_m,
+        p_i_max=p_i_max,
+        p_inf_co=crossover_pressure_altitude(tup.MMO, p_i_max),
     )
     return icao, params
 
@@ -156,22 +178,6 @@ def load_aircraft_engine_params() -> Mapping[str, PSAircraftEngineParams]:
         "Type": object,
         "n_engine": int,
         "winglets": object,
-        "Sref_m2": float,
-        "delta_2": float,
-        "cos_sweep": float,
-        "AR": float,
-        "psi_0": float,
-        "Xo": float,
-        "wing_constant": float,
-        "j_2": float,
-        "j_1": float,
-        "mf_idle_SLS_kg_s": float,
-        "mf_max_T_O_SLS_kg_s": float,
-        "nominal_F00_ISA_kn": float,
-        "M_des": float,
-        "CT_des": float,
-        "eta_1": float,
-        "eta_2": float,
         "WV": object,
         "MTOM_kg": float,
         "MLM_kg": float,
@@ -181,13 +187,104 @@ def load_aircraft_engine_params() -> Mapping[str, PSAircraftEngineParams]:
         "MZFM_MTOM": float,
         "OEM_i_MTOM": float,
         "MPM_i_MTOM": float,
-        "etaL_D_do": float,
-        "MaxAlt_ft": float,
-        "MMO": float,
+        "Sref_m2": float,
         "span_m": float,
         "bf_m": float,
+        "delta_2": float,
+        "cos_sweep": float,
+        "AR": float,
+        "psi_0": float,
+        "Xo": float,
+        "wing_constant": float,
+        "j_2": float,
+        "j_1": float,
+        "nominal_F00_ISA_kn": float,
+        "mf_max_T_O_SLS_kg_s": float,
+        "mf_idle_SLS_kg_s": float,
+        "M_des": float,
+        "CT_des": float,
+        "eta_1": float,
+        "eta_2": float,
+        "Mec": float,
+        "Tec": float,
+        "TET_max_climb_K": float,
+        "FL_max": float,
+        "MMO": float,
     }
 
     df = pd.read_csv(PS_FILE_PATH, dtype=dtypes)
 
     return dict(_row_to_aircraft_engine_params(tup) for tup in df.itertuples(index=False))
+
+
+def impact_pressure_max_operating_limits(max_mach_num: float) -> float:
+    """
+    Calculate maximum permitted operational impact pressure.
+
+    Parameters
+    ----------
+    max_mach_num: float
+        Maximum permitted operational Mach number for aircraft type.
+
+    Returns
+    -------
+    float
+        Maximum permitted operational impact pressure for aircraft type, `p_i_max`, [:math:`Pa`]
+
+    Notes
+    -----
+    The impact pressure is the difference between the free stream total pressure `p_0` and the
+    atmospheric static pressure `p_inf`. By definition, the calibrated airspeed, `v_cas`, is
+    the speed at sea level in the ISA that has the same impact pressure.
+    """
+    v_cas_mo_over_c_msl = max_calibrated_airspeed_over_speed_of_sound(max_mach_num)
+    return c.p_surface * (
+        (1 + 0.5 * (c.kappa - 1) * v_cas_mo_over_c_msl**2) ** (c.kappa / (c.kappa - 1)) - 1
+    )
+
+
+def max_calibrated_airspeed_over_speed_of_sound(max_mach_num: float) -> float:
+    """
+    Calculate max calibrated airspeed over the speed of sound at ISA mean sea level.
+
+    Parameters
+    ----------
+    max_mach_num: float
+        Maximum permitted operational Mach number for aircraft type.
+
+    Returns
+    -------
+    float
+        Ratio of maximum operating limits of the calibrated airspeed (CAS) over the speed of sound
+        at mean sea level (MSL) in standard atmosphere (ISA), ``v_cas_mo_over_c_msl``
+    """
+    return 0.57 * (max_mach_num + 0.10)
+
+
+def crossover_pressure_altitude(max_mach_num: float, p_i_max: float) -> float:
+    """
+    Calculate crossover pressure altitude.
+
+    Parameters
+    ----------
+    max_mach_num: float
+        Maximum permitted operational Mach number for aircraft type.
+    p_i_max : float
+        Maximum permitted operational impact pressure for aircraft type, [:math:`Pa`]
+
+    Returns
+    -------
+    float
+        Crossover pressure altitude, `p_inf_co`, [:math:`Pa`]
+
+    Notes
+    -----
+    At low altitudes, the calibrated airspeed (CAS) is used to determine the maximum aircraft
+    operational speed. However, the ambient temperature drops as altitude increases, which causes
+    the Mach number to increase even when the CAS remains constant. This can cause the Mach number
+    to exceed the maximum permitted operational Mach number. Therefore, above the crossover
+    altitude, the maximum operational speed is determined by the Mach number instead of CAS.
+    """
+    return p_i_max / (
+        0.5 * c.kappa * max_mach_num**2 * (1 + (max_mach_num**2 / 4) + (max_mach_num**4 / 40))
+    )
