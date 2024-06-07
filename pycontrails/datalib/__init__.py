2e6bd2b1b8982325e4956ad9c27fe3d8fa8b19c7
Marc Shapiro <git@ms.tengr.net>
2024-06-06 23:31:52 -0400

    Add new modules to docstring

--- pycontrails/datalib/__init__.py
+++ pycontrails/datalib/__init__.py
@@ -1,9 +1,12 @@
 """
-Meteorology and Air Traffic data source wrappers.
+Meteorology, Air Traffic, and Observation data source wrappers.
 
 See individual modules for met variables and additional exports.
 
 - :module:`pycontrails.datalib.ecmwf`
 - :module:`pycontrails.datalib.gfs`
 - :module:`pycontrails.datalib.spire`
+- :module:`pycontrails.datalib.goes`
+- :module:`pycontrails.datalib.landsat`
+- :module:`pycontrails.datalib.sentinel`
 """
