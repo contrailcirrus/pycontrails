91bd2c0e0acedad513ba49368e2c728d3505230a
Marc Shapiro <git@ms.tengr.net>
2024-06-06 20:29:36 -0400

    Add compare_cocip_with_goes to cocip import

--- pycontrails/models/cocip/__init__.py
+++ pycontrails/models/cocip/__init__.py
@@ -4,6 +4,7 @@ from pycontrails.models.cocip.cocip import Cocip
 from pycontrails.models.cocip.cocip_params import CocipFlightParams, CocipParams
 from pycontrails.models.cocip.cocip_uncertainty import CocipUncertaintyParams, habit_dirichlet
 from pycontrails.models.cocip.output_formats import (
+    compare_cocip_with_goes,
     contrail_flight_summary_statistics,
     contrails_to_hi_res_grid,
     flight_waypoint_summary_statistics,
@@ -24,4 +25,5 @@ __all__ = [
     "longitude_latitude_grid",
     "natural_cirrus_properties_to_hi_res_grid",
     "time_slice_statistics",
+    "compare_cocip_with_goes",
 ]
