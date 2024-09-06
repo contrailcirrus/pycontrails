4cf2e0fb9cebc59d6c6d46872fe8d2cbda6bb856
Zeb Engberg <zebengberg@gmail.com>
2024-09-06 15:26:45 -0600

    cleanup and extend fixture generation script

--- tests/fixtures/ecmwf-met.py
+++ tests/fixtures/ecmwf-met.py
@@ -1,8 +1,10 @@
 """Test Fixtures.
 
-Create test data for pycontrails.
+Create test data for pycontrails. The data itself is committed to the repository,
+so this script only needs to be run once. Because ECMWF updates their reanalysis
+data from time to time, re-running this script will alter the data.
 
-See output in `pycontrails/tests/static`
+See output in ``pycontrails/tests/unit/static``.
 """
 
 # import logging
@@ -10,19 +12,18 @@ See output in `pycontrails/tests/static`
 # logging.basicConfig(level=logging.DEBUG)
 
 import pathlib
-import sys
 from datetime import datetime
 
 import xarray as xr
 
 from pycontrails import DiskCacheStore
-from pycontrails.datalib.ecmwf import ERA5
+from pycontrails.datalib.ecmwf import ERA5, ERA5ModelLevel
 
 # use separate disk cache
 disk_cache = DiskCacheStore(cache_dir="cache")
 
 # paths
-parent = pathlib.Path(sys.argv[0]).parents[1].absolute()
+parent = pathlib.Path(__file__).parents[1].absolute()
 static_dir = parent / "unit" / "static"
 
 # domain
@@ -30,6 +31,7 @@ times = (datetime(2019, 5, 31, 5, 0, 0), datetime(2019, 5, 31, 6, 0, 0))
 pl_variables = ["air_temperature", "specific_humidity", "specific_cloud_ice_water_content"]
 sl_variables = ["surface_air_pressure"]
 pressure_levels = [300, 250, 225]
+step = 100  # used for downselecting the domain
 
 ### ERA5 PL data
 
@@ -39,27 +41,55 @@ era5pl = ERA5(
     pressure_levels=pressure_levels,
     cachestore=disk_cache,
 )
-_ = era5pl.open_metdataset(xr_kwargs={"parallel": False})
+era5pl.download()
 
 # NOTE we are saving the cached CDS *source* files here.
 # These are not the same as purely raw downloaded files from CDS.
 ds_pl = xr.open_mfdataset(era5pl._cachepaths)
-step = 100
+ds = ds_pl.isel(latitude=slice(0, None, step), longitude=slice(0, None, step))
 
-ds = ds_pl.reindex(
-    latitude=ds_pl["latitude"].values[0::step], longitude=ds_pl["longitude"].values[0::step]
-)
-ds.to_netcdf(static_dir / "met-ecmwf-pl.nc")
+target = static_dir / "met-ecmwf-pl.nc"
+if not target.is_file():
+    ds.to_netcdf(target)
 
 
 ### ERA5 Single Level
 era5sl = ERA5(times, variables=sl_variables, cachestore=disk_cache)
-_ = era5sl.open_metdataset(xr_kwargs={"parallel": False})
+era5sl.download()
 
 # NOTE we are saving the raw CDS *source* files here
 ds_sl = xr.open_mfdataset(era5sl._cachepaths)
-step = 100
-ds = ds_sl.reindex(
-    latitude=ds_sl["latitude"].values[0::step], longitude=ds_sl["longitude"].values[0::step]
+ds = ds_sl.isel(latitude=slice(0, None, step), longitude=slice(0, None, step))
+
+target = static_dir / "met-ecmwf-sl.nc"
+if not target.is_file():
+    ds.to_netcdf(target)
+
+
+### Model level data
+era5ml = ERA5ModelLevel(
+    times[0],  # download a single time to save space
+    variables=pl_variables,
+    model_levels=list(range(74, 85)),
+    pressure_levels=pressure_levels,
+    cachestore=disk_cache,
+    cache_raw=True,
 )
-ds.to_netcdf(static_dir / "met-ecmwf-sl.nc")
+era5ml.download()
+
+lnsp_name = "era5ml-bdb942b20d6a170e4c0240715101e6c7-raw.nc"
+ds = xr.open_mfdataset(disk_cache.path(lnsp_name))
+ds = ds.isel(latitude=slice(0, None, step), longitude=slice(0, None, step))
+
+target = static_dir / "met-ecmwf-lnsp.nc"
+if not target.is_file():
+    ds.to_netcdf(target)
+
+
+ml_name = "era5ml-2ad0995e9616aa91c702bf6c3abe752f-raw.nc"
+ds = xr.open_mfdataset(disk_cache.path(ml_name))
+ds = ds.isel(latitude=slice(0, None, step), longitude=slice(0, None, step))
+
+target = static_dir / "met-ecmwf-ml.nc"
+if not target.is_file():
+    ds.to_netcdf(target)
