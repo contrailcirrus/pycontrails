55dcf3b892bb8235882d1de86c6dd0ebab0fa816
Tom Dean <thomas.dean@breakthroughenergy.org>
2024-10-23 12:10:01 +0200

    Add ps_nominal_optimize_mach as import

--- pycontrails/models/ps_model/__init__.py
+++ pycontrails/models/ps_model/__init__.py
@@ -4,7 +4,7 @@ from pycontrails.models.ps_model.ps_aircraft_params import (
     PSAircraftEngineParams,
     load_aircraft_engine_params,
 )
-from pycontrails.models.ps_model.ps_grid import PSGrid, ps_nominal_grid
+from pycontrails.models.ps_model.ps_grid import PSGrid, ps_nominal_grid, ps_nominal_optimize_mach
 from pycontrails.models.ps_model.ps_model import PSFlight, PSFlightParams
 
 __all__ = [
@@ -14,4 +14,5 @@ __all__ = [
     "PSGrid",
     "load_aircraft_engine_params",
     "ps_nominal_grid",
+    "ps_nominal_optimize_mach"
 ]
