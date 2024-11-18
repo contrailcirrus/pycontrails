392b16a1ac8fdca1c7805f0b850d977da1214a25
Zeb Engberg <zebengberg@gmail.com>
2024-11-18 17:35:34 -0500

    update copyright range 2023 -> present

--- pycontrails/__init__.py
+++ pycontrails/__init__.py
@@ -1,7 +1,7 @@
 """
 ``pycontrails`` public API.
 
-Copyright 2021-2023 Breakthrough Energy
+Copyright 2021-present Breakthrough Energy
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
