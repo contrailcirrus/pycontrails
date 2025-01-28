cd4f97ed73b6cb20b99b0acd6ccf03e2b3459d95
Zeb Engberg <zebengberg@gmail.com>
2025-01-28 07:31:00 -0700

    create spire directory

---
+++ pycontrails/datalib/spire/__init__.py
@@ -0,0 +1,5 @@
+"""`Spire Aviation <https://spire.com/aviation/>`_ ADS-B data support."""
+
+from pycontrails.datalib.spire.spire import ValidateTrajectoryHandler
+
+__all__ = ["ValidateTrajectoryHandler"]
