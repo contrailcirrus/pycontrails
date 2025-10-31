b87e1f067fbf5940e7c79e83e51242c25721196a
Tristan Abbott <tristanhabbott@gmail.com>
2025-10-30 20:40:46 -0500

    wip: iagos datalib

--- pycontrails/datalib/iagos.py
+++ pycontrails/datalib/iagos.py
@@ -1,15 +1,14 @@
 """IAGOS utilties."""
 
-import logging
 import os
 import pathlib
-import traceback
+import re
 import warnings
 
 import numpy as np
 import xarray as xr
 
-from pycontrails.core import MetVariable, cache, met_var
+from pycontrails.core import Flight, MetVariable, cache, met_var
 from pycontrails.datalib._met_utils import metsource
 from pycontrails.utils.types import DatetimeLike
 
@@ -27,14 +26,32 @@ WaterVaporMoleFraction = MetVariable(
     description="The number of molecules of water per molecule of air.",
 )
 
-SUPPORTED_VARIABLES = [
+
+#: Variables measured by aircraft directly.
+#: These variables are measured on all IAGOS flights and are accompanied
+#: by validity flags.
+AIRCRAFT_VARIABLES = [
     met_var.AirPressure,
     Altitude,
     met_var.EastwardWind,
     met_var.NorthwardWind,
     met_var.AirTemperature,
-    WaterVaporMoleFraction,
 ]
+
+
+#: Variables measured by IAGOS instruments.
+#: These variables are not measured on all IAGOS flights.
+#: When present, they are accompanied by validity and processing flags.
+IAGOS_VARIABLES = [WaterVaporMoleFraction]
+
+
+def extract_flight_id(filename: str) -> str:
+    """Extract IAGOS flight id."""
+    match = re.fullmatch(r"IAGOS_timeseries_(\d{8})(\d{8})_L2_(\d.\d.\d).nc4", filename)
+    if match is None:
+        msg = f"Name of IAGOS file {filename} does not match expected format."
+        raise ValueError(msg)
+    return match.group(1) + match.group(2)
 
 
 class IAGOS:
@@ -55,6 +72,45 @@ class IAGOS:
     cachestore: cache.CacheStore | None, optional
         Cache data store for retrieved IAGOS data files. Defaults of :class:`cache.DiskCacheStore`.
         If set to None, retrieved files are loaded directly into memory without caching.
+
+    Notes
+    -----
+    IAGOS files are expected to be named as follows::
+
+        IAGOS_timeseries_{YYYYmmdd}{XXXXXXXX}_L2_{V.V.V}.nc4
+
+    - {YYYYmmdd} is the flight departure date
+    - {XXXXXXXX} is an 8-digit flight identifier
+    - {V.V.V} is the file version
+
+    {YYYYmmmdd}{XXXXXXXX} is used as a unique identifier for IAGOS flights.
+
+    IAGOS files are expected to contain a single datetime-like dimension coordinate
+    called "UTC_time". In addition, they are expected to contain the following
+    non-dimension coordinates:
+
+    - Longitude (variable: "lon", units: "degree_east")
+    - Latitude (variable: "lat", units: "degree_north")
+    - Barometric altitude (variable: "baro_alt_AC", units: "m")
+
+    and the following one-dimensional variables:
+
+    - Geometric altitude (standard_name: "gps_altitude", units: "m")
+    - Air pressure (standard_name: "air_pressure", units: "Pa")
+    - Air temperature (standard_name: "air_temperature", units: "K")
+    - Zonal wind (standard_name: "eastward_wind", units: "m s-1")
+    - Meridional wind (standard_name: "northward_wind", unit: "m s-1")
+
+    IAGOS files *may* (but are not guaranteed to) contain the following
+    one-dimensional variables, along with associated standard errors (variable suffix "_error")
+    and processing flags (variable suffix "_process_flag").
+
+    - Water vapor mole fraction (standard_name: "mole_fraction_of_water_vapor", units: "ppm")
+
+    All variables and non-dimension coordinates are expected to have "standard_name" and
+    "units" attributes, and all variables and non-dimension coordinates except latitude and
+    longitude are expected to have associated validity flags (variable suffix "_validity_flag).
+
     """
 
     __marker = object()
@@ -77,15 +133,6 @@ class IAGOS:
         self.timespan = metsource.parse_timesteps(time, freq=None)
         self.variables = metsource.parse_variables(variables or [], self.supported_variables)
 
-        if paths:
-            filenames = [os.path.basename(p) for p in paths]
-            duplicates = [f for f in set(filenames) if filenames.count(f) > 1]
-            if duplicates:
-                msg = (
-                    f"IAGOS filenames {duplicates} appear more than once in `paths`. "
-                    f"Deduplicate files included in `paths` before using."
-                )
-                raise ValueError(msg)
         self.paths = [str(p) for p in paths] if paths else None
 
     def __repr__(self) -> str:
@@ -120,65 +167,7 @@ class IAGOS:
             Note that not all IAGOS flights are guaranteed
             to record all available parameters.
         """
-        return SUPPORTED_VARIABLES
-
-    def _filter(self, path: str) -> bool:
-        """Filter local IAGOS files."""
-        try:
-            ds = xr.open_dataset(path, decode_cf=False)
-
-            # check if flight includes time coordinate
-            try:
-                # decode_cf requires a Dataset
-                t = xr.decode_cf(ds[["UTC_time"]])["UTC_time"]
-                assert t.dtype.kind == "M"
-            except Exception:
-                logging.debug(f"Could not decode 'UTC_time' coordinate from {path}")
-                return False
-
-            # check if flight overlaps target timepan
-            if self.timespan:
-                start, end = self.timespan
-                if not ((t >= np.datetime64(start)) & (t <= np.datetime64(end))).any():
-                    logging.debug(f"No overlap between target time range and {path}")
-                    return False
-
-            # get list of available variables
-            present = {
-                (v.attrs["standard_name"], v.dims, v.attrs["units"]) for v in ds.variables.values()
-            }
-
-            # check for required coordinate variables
-            required = {
-                ("longitude", ("UTC_time",), "degree_east"),
-                ("latitude", ("UTC_time",), "degree_north"),
-                ("barometric_altitude", ("UTC_time",), "m"),
-            }
-            missing = required.difference(present)
-            if missing:
-                logging.debug(f"Coordinates {[m[0] for m in missing]} missing from {path}")
-                return False
-
-            # check for required data variables
-            required = {
-                # IAGOS files don't include ** in unit exponents
-                (v.standard_name, ("UTC_time",), (v.units or "1").replace("*", ""))
-                for v in self.variables
-            }
-            missing = required.difference(present)
-            if missing:
-                logging.debug(f"Variables {[m[0] for m in missing]} missing from {path}")
-                return False
-
-            return True
-
-        except Exception:
-            msg = (
-                f"Unexpected error while processing IAGOS data file at {path}. "
-                f"Traceback: {traceback.format_exc()}"
-            )
-            warnings.warn(msg)
-            return False
+        return AIRCRAFT_VARIABLES + IAGOS_VARIABLES
 
     def list_files(self) -> list[str]:
         """List available files.
@@ -186,11 +175,29 @@ class IAGOS:
         Returns
         -------
         list[str]
-            List of available IAGOS files. Will be a subset of files in :attr:`paths` if defined,
+            List of available IAGOS files. Will return the files in :attr:`paths` if defined,
             or a list of filenames available in the IAGOS database otherwise.
+
+        Raises
+        ------
+        ValueError
+            If :attr:`paths` is defined and includes multiple filenames with the same
+            flight id.
+
+        IAGOSProcessingError
+            If :attr:`paths` is defined and includes a filename that does not match
+            the expected format.
         """
         if self.paths:
-            return sorted([os.path.basename(p) for p in self.paths if self._filter(p)])
+            flight_ids = [extract_flight_id(os.path.basename(p)) for p in self.paths]
+            duplicates = [f for f in set(flight_ids) if flight_ids.count(f) > 1]
+            if duplicates:
+                msg = (
+                    f"IAGOS flight ids {duplicates} appear more than once in `paths`. "
+                    f"Deduplicate files included in `paths` before using."
+                )
+                raise ValueError(msg)
+            return sorted([os.path.basename(p) for p in self.paths])
 
         msg = (
             "File retrieval from the IAGOS database has not been implemented. "
@@ -221,3 +228,140 @@ class IAGOS:
             "To use this datalib, provide paths to local IAGOS files using the `paths` parameter."
         )
         raise NotImplementedError(msg)
+
+    def load_flights(self) -> list[Flight]:
+        """Process IAGOS data and return as a list of flights.
+
+        Returns
+        -------
+        list[Flight]
+            Processed IAGOS data.
+        """
+        flights = []
+
+        for filename in self.list_files():
+            ds = self.get(filename)
+            if not self._includes_time_and_variables(ds):
+                continue
+
+            flight = self._create_flight(ds)
+            flight_id = extract_flight_id(filename)
+            flight.attrs["flight_id"] = flight_id
+            flights.append(flight)
+
+        return flights
+
+    def _includes_time_and_variables(self, ds: xr.Dataset) -> bool:
+        """Check if IAGOS file includes requested time and variables.
+
+        This method will issue a warning and return ``False`` if problems
+        with the contents of the IAGOS file are detected.
+        """
+        # check for presence of time coordinate
+        if "UTC_time" not in ds.coords:
+            msg = "IAGOS data file does not contain time coordinate"
+            warnings.warn(msg)
+            return False
+        if ds["UTC_time"].dtype.kind != "M":
+            msg = "IAGOS time coordinate is not datetime-like"
+            warnings.warn(msg)
+            return False
+
+        # check if flight overlaps target timepan
+        if self.timespan:
+            start, end = self.timespan
+            t = ds["UTC_time"]
+            if not ((t >= np.datetime64(start)) & (t <= np.datetime64(end))).any():
+                return False
+
+        present = {
+            v.attrs["standard_name"]: k
+            for k, v in ds.variables.items()
+            if "standard_name" in v.attrs
+        }
+        for variable in self.variables:
+            # check if dataset includes variable
+            if variable.standard_name not in present:
+                return False
+
+            # once presence of variable is established, check units and
+            # presence of flags and errors.
+            # raise an error if expected variables are not found.
+            key = present[variable.standard_name]
+            # IAGOS units don't include "**"
+            if variable.units and ds[key].attrs["units"] != variable.units.replace("*", ""):
+                msg = f"IAGOS variable {key} does not have expected units"
+                warnings.warn(msg)
+                return False
+            flag = f"{key}_validity_flag"
+            if flag not in ds:
+                msg = f"IAGOS variable {key} does not have a validity flag"
+                warnings.warn(msg)
+                return False
+
+            if variable not in IAGOS_VARIABLES:
+                continue
+
+            error = f"{key}_error"
+            if error not in ds:
+                msg = f"IAGOS variable {key} does not have a standard error"
+                warnings.warn(msg)
+                return False
+            # IAGOS units don't include "**"
+            if variable.units and ds[error].attrs["units"] != variable.units.replace("*", ""):
+                msg = f"IAGOS variable {key} standard error does not have expected units"
+                warnings.warn(msg)
+                return False
+            flag = f"{key}_process_flag"
+            if flag not in ds:
+                msg = f"IAGOS variable {key} does not have a processing flag"
+                warnings.warn(msg)
+                return False
+
+        # check for presence of other coordinates
+        for coord, units in (("lon", "degree_east"), ("lat", "degree_north"), ("baro_alt_AC", "m")):
+            if coord not in ds.coords:
+                msg = f"IAGOS data file does not contain coordinate {coord}"
+                warnings.warn(msg)
+                return False
+            if ds[coord].units != units:
+                msg = f"IAGOS coordinate {coord} does not have expected units"
+                warnings.warn(msg)
+                return False
+        flag = "baro_alt_AC_validity_flag"
+        if flag not in ds:
+            msg = "IAGOS variable baro_alt_AC does not have a validity flag"
+            warnings.warn(msg)
+            return False
+
+        return True
+
+    def _create_flight(self, ds: xr.Dataset) -> Flight:
+        """Create a flight contents of IAGOS data file.
+
+        Should be called after first using _includes_time_and_variables
+        to check that required variables are present.
+        """
+        # set coordinates
+        flight = Flight(
+            longitude=ds["lon"], latitude=ds["lat"], altitude=ds["baro_alt_AC"], time=ds["UTC_time"]
+        )
+        flight["altitude_validity_flag"] = ds["baro_alt_AC_validity_flag"]
+
+        # add variables
+        present = {
+            v.attrs["standard_name"]: k
+            for k, v in ds.variables.items()
+            if "standard_name" in v.attrs
+        }
+        for variable in self.variables:
+            name = variable.standard_name
+            key = present[name]
+            flight[name] = ds[key]
+            flight[f"{name}_validity_flag"] = ds[f"{key}_validity_flag"]
+            if variable not in IAGOS_VARIABLES:
+                continue
+            flight[f"{name}_standard_error"] = ds[f"{key}_error"]
+            flight[f"{name}_process_flag"] = ds[f"{key}_process_flag"]
+
+        return flight
