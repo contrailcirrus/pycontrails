2195a3c6708a13ba7433874cfea96247d38c0ca2
Zeb Engberg <zebengberg@gmail.com>
2026-01-26 09:57:41 -0700

    make particle types immutable, add annotation

--- pycontrails/models/cocip/cocip_params.py
+++ pycontrails/models/cocip/cocip_params.py
@@ -14,8 +14,8 @@ from pycontrails.core.aircraft_performance import AircraftPerformance
 from pycontrails.core.models import AdvectionBuffers
 from pycontrails.models.emissions import Emissions
 from pycontrails.models.emissions.emissions import EmissionsParams
-from pycontrails.models.humidity_scaling import HumidityScaling
 from pycontrails.models.extended_k15 import Particle, ParticleType
+from pycontrails.models.humidity_scaling import HumidityScaling
 
 
 def _radius_threshold_um() -> npt.NDArray[np.float32]:
@@ -246,19 +246,19 @@ class CocipParams(AdvectionBuffers):
     vpm_activation: bool = False
 
     #: Particle types (low nvPM)
-    particles_low_nvpm = [
+    particles_low_nvpm: tuple[Particle, ...] = (
         Particle(type=ParticleType.NVPM, kappa=0.005, gmd=30.0e-9, gsd=2.0),
         Particle(type=ParticleType.VPM, kappa=0.6, gmd=1.0e-9, gsd=1.50, ei_vpm=1e17),  # Sulfur
         Particle(type=ParticleType.VPM, kappa=0.01, gmd=5.0e-9, gsd=1.40, ei_vpm=1e15),  # Oil
         Particle(type=ParticleType.AMBIENT, kappa=0.5, gmd=30.0e-9, gsd=2.3, n_ambient=600.0e6),
-    ]
+    )
 
     # Particle types (high nvPM)
-    particles_hi_nvpm = [
+    particles_hi_nvpm: tuple[Particle, ...] = (
         Particle(type=ParticleType.NVPM, kappa=0.005, gmd=30.0e-9, gsd=2.0),
         Particle(type=ParticleType.VPM, kappa=0.6, gmd=1.0e-9, gsd=1.50, ei_vpm=1e17),  # Sulfur
         Particle(type=ParticleType.AMBIENT, kappa=0.5, gmd=30.0e-9, gsd=2.3, n_ambient=600.0e6),
-    ]
+    )
 
     #: Experimental. Radiative effects due to contrail-contrail overlapping
     #: Account for change in local contrail shortwave and longwave radiative forcing
