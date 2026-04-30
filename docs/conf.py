68866a3d4e989fad39c2cbe6da1fb350b78a5b1a
Marc Shapiro <git@ms.tengr.net>
2026-04-30 16:41:35 -0400

    Update favicon to by light/dark dynamic

--- docs/conf.py
+++ docs/conf.py
@@ -260,7 +260,7 @@ html_theme_options = {
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 # html_logo = "_static/img/logo.jpg"
-html_favicon = "_static/img/favicon.png"
+html_favicon = "_static/img/favicon.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
