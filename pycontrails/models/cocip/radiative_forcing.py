fd5e2228c65f79873bf39d06d01288612462f7db
Zeb Engberg <zebengberg@gmail.com>
2026-08-10 06:08:21 -0600

    tweak type: ignore for numpy 2.5.2

--- pycontrails/models/cocip/radiative_forcing.py
+++ pycontrails/models/cocip/radiative_forcing.py
@@ -287,7 +287,7 @@ def effective_radius_by_habit(
         effective_radius_myhre,
         0.0,
     ]
-    return np.piecewise(r_vol_um, cond_list, func_list)  # type: ignore[call-overload]
+    return np.piecewise(r_vol_um, cond_list, func_list)  # type: ignore[arg-type]
 
 
 def effective_radius_sphere(r_vol_um: npt.NDArray[np.floating]) -> npt.NDArray[np.floating]:
